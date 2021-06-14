7e2bbbbdb43d4ab8214f43b0042b10085159e6b8
MariaAnggreniTako <85872366+MariaAnggreniTako@users.noreply.github.com>
2021-06-14 18:02:32 +0700

    Add files via upload

---
+++ Apl5D.py
@@ -0,0 +1,24 @@
+"""
+created by Maria Anggreni Tako (20083000087)
+    kelas 2C
+"""
+jwb = "y"
+
+while jwb=="y":
+    print("------------------")
+    print("CEK Nilai Mahasiswa")
+    print("------------------")
+    n = input("Masukkan Nilai = ")
+    x= int(n)
+    if x>=91 & x<=100:
+        status = "A"
+    elif x>=81 & x<91:
+        status = "B"
+    elif x>=71 & x<81:
+        status = "C"
+    else:
+        status="D"
+    print (status)
+    jwb = input(">> Mau mengulangi ? y/t = ")
+    if jwb=="t":
+        break
